fd4e986ba52bbd081c3f665412bd5d3281b00804
keizie <github@keizie.net>
2022-07-02 21:31:42 +0900

    Add nest-next package
    - tsconfig.json set noEmit: false (was true just after setup)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Render, Res } from '@nestjs/common';
+import { RenderableResponse } from 'nest-next';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +7,22 @@ export class AppController {
   constructor(
     private readonly appService: AppService,
   ) {}
+
+  @Get()
+  @Render('Index')
+  public index() {
+    // initial props
+    return {
+      title: 'Next with Nest',
+    };
+  }
+
+  @Get('2')
+  public index2(@Res() res: RenderableResponse) {
+    res.render('Index', {
+      title: 'Next with Nest',
+    });
+  }
 
   @Get('hello')
   getHello(): string {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import Next from 'next';
+import { RenderModule } from 'nest-next';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventModule } from './event/event.module';
+import { resolve } from 'path';
 
 @Module({
   imports: [
+    RenderModule.forRootAsync(
+      Next({
+        dev: process.env.NODE_ENV !== 'production',
+        dir: resolve(__dirname, '..'),
+      }),
+      { passthrough404: true, viewsDir: null },
+    ),
     EventModule,
   ],
   controllers: [AppController],
